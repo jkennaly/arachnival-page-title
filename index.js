123be83fb85fd7b02ed16d35297bc06585dc84db
Jason Kennaly <jkennaly@gmail.com>
2022-01-11 14:26:16 -0800

    triggered by Sns

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const chromium = require('chrome-aws-lambda');
+const redis = require("redis");
+
+const client = redis.createClient({url: process.env.REDIS_URL});
 
 exports.handler = async (event, context, callback) => {
     let lineupUrl = "https://festigram.app/site/festivals/Coachella/2020"
     let responseCode = 200;
     console.log("request: " + JSON.stringify(event));
-    
-    if (event.queryStringParameters && event.queryStringParameters.lineupUrl) {
+
+    if(event.Records && event.Records[0] && event.Records[0].Sns && event.Records[0].Sns.Message) {
+    	try {
+    		const parsedMessage = JSON.parse(event.Records[0].Sns.Message)
+    		lineupUrl = parsedMessage.lineupUrl
+    	} catch (err) {
+    		console.error('Sns Message parse error')
+    		console.error(err)
+    	}
+    } else if (event.queryStringParameters && event.queryStringParameters.lineupUrl) {
         lineupUrl = event.queryStringParameters.lineupUrl;
     }
-    
-    if (event.body) {
-        let body = JSON.parse(event.body)
-        if (body.festid) 
-            festid = body.festid;
-    }
+  const leKey = 'arach-lineup.' + lineupUrl
   let result = null;
   let browser = null;
 
   try {
-  	//await chromium.font('./.fonts/NotoColorEmoji.ttf');
+  	await chromium.font('./.fonts/NotoColorEmoji.ttf');
     browser = await chromium.puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
@@ -40,5 +46,8 @@ exports.handler = async (event, context, callback) => {
     }
   }
   console.log('Spidey sensed', result)
+  await client.set(leKey, JSON.stringify(result), {
+		EX: 3600 * 24 * 30
+	})
   return callback(null, result);
 };
